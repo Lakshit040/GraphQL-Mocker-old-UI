2a12085967f833d0d5b136dc8941627f5e947bbd
Priyansh Rathi <priyansh.rathi@sprinklr.com>
2023-06-14 00:42:07 +0530

    Implement basic response mocking with chrome debugger

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,16 +1,26 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 import { MessageType } from "../common/types";
+
+import { startMocking, stopMocking } from "../core/devtools";
 
 function App() {
   useEffect(() => {
     chrome.runtime.sendMessage({ type: MessageType.PanelMounted, data: {tabId: chrome.devtools.inspectedWindow.tabId} });
   }, []);
 
+  const [capturing, setCapturing] = useState(false);
+
+  function handleClick() {
+    (capturing ? stopMocking() : startMocking()).then(() => setCapturing(c => !c));
+  }
+
   return (
-    <div className="App">
-      Hello World
+    <div className={`App App${capturing ? "Stop" : "Start"}Capturing`} onClick={handleClick}>
+      <div className="CaptureStatus">
+        {capturing ? "Stop" : "Start"} Capturing
+      </div>
     </div>
   );
 }
