a03340874bb1c8e6caa12d20ea72ceb22a8d88ab
Priyansh Rathi <priyansh.rathi@sprinklr.com>
2023-06-15 15:41:23 +0530

    Format changes from the previous commit

--- src/ui/InputComponent.tsx
+++ src/ui/InputComponent.tsx
@@ -1,71 +1,96 @@
-import {useState} from 'react';
+import { useState } from "react";
 import "./InputComponent.css";
-import { GraphQLOperationType } from '../common/types';
+import { GraphQLOperationType } from "../common/types";
 
-function startMocking(operationType: GraphQLOperationType, operationName: string, sampleResponse: string) {
-    console.log(operationType);
-    console.log(operationName);
-    console.log(sampleResponse);
+function startMocking(
+  operationType: GraphQLOperationType,
+  operationName: string,
+  sampleResponse: string
+) {
+  console.log(operationType);
+  console.log(operationName);
+  console.log(sampleResponse);
 }
 
 export default function InputComponent() {
-    const [op, setOp] = useState(GraphQLOperationType.Query);
-    const [name, setName] = useState('');
-    const [res, setRes] = useState('');
+  const [op, setOp] = useState(GraphQLOperationType.Query);
+  const [name, setName] = useState("");
+  const [res, setRes] = useState("");
 
-    const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        if(event.target.value === 'query'){
-            setOp(GraphQLOperationType.Query);
-        }
-        else{
-            setOp(GraphQLOperationType.Mutation);
-        }
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    if (event.target.value === "query") {
+      setOp(GraphQLOperationType.Query);
+    } else {
+      setOp(GraphQLOperationType.Mutation);
     }
+  };
 
-    const handleResponseChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setRes(event.target.value);
-    };
+  const handleResponseChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setRes(event.target.value);
+  };
 
-    const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    };
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
-    return (
-        <>
-            <div className="row">
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <div className="form-group">
+            <label className="label" htmlFor="dropdown">
+              Operation
+            </label>
 
-                <div className="col">
+            <select
+              value={op === GraphQLOperationType.Query ? "query" : "mutation"}
+              className="input-field"
+              id="dropdown"
+              onChange={handleOptionChange}
+            >
+              <option value="query">Query</option>
+              <option value="mutation">Mutation</option>
+            </select>
 
-                    <div className="form-group">
+            <label className="label nameLabel" htmlFor="textInput">
+              Operation Name
+            </label>
+            <input
+              type="text"
+              className="input-field"
+              id="textInput"
+              value={name}
+              onChange={handleNameChange}
+            />
 
-                        <label className="label" htmlFor="dropdown">Operation</label>
+            <label className="label nameLabel" htmlFor="textarea">
+              Sample Response
+            </label>
 
-                        <select value={op === GraphQLOperationType.Query ? "query" : "mutation"} className="input-field" id="dropdown" onChange={handleOptionChange}>
-                            <option value='query'>Query</option>
-                            <option value='mutation'>Mutation</option>
-                        </select>
+            <textarea
+              className="input-field"
+              id="textarea"
+              rows={4}
+              value={res}
+              onChange={handleResponseChange}
+            ></textarea>
+            <button
+              className="button"
+              onClick={() => startMocking(op, name, res)}
+            >
+              Generate Mock Response
+            </button>
+          </div>
+        </div>
+      </div>
 
-                        <label className="label nameLabel" htmlFor="textInput">Operation Name</label>
-                        <input type="text" className="input-field" id="textInput" value={name} onChange={handleNameChange}/>
-
-                        <label className="label nameLabel" htmlFor="textarea">Sample Response</label>
-
-                        <textarea className="input-field" id="textarea" rows={4} value={res} onChange={handleResponseChange}>
-
-                        </textarea>
-                        <button className="button" onClick={() => startMocking(op, name, res)}>Generate Mock Response</button>
-
-                    </div>
-                </div>
-            </div>
-
-            <div className="row">
-                <div className="col text-center">
-                    <h2>Generated Mock Response</h2>
-                </div>
-            </div>
-
-        </>
-    );
+      <div className="row">
+        <div className="col text-center">
+          <h2>Generated Mock Response</h2>
+        </div>
+      </div>
+    </>
+  );
 }
-
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import InputComponent from './InputComponent';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import InputComponent from "./InputComponent";
 
-const root = document.createElement("div")
-root.className = "container"
-document.body.appendChild(root)
+const root = document.createElement("div");
+root.className = "container";
+document.body.appendChild(root);
 const rootDiv = ReactDOM.createRoot(root);
 rootDiv.render(
   <React.StrictMode>
