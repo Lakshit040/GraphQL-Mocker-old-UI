81df89c62decb560ec1bc28718f51959bc404d4b
Priyansh Rathi <priyansh.rathi@sprinklr.com>
2023-06-16 21:11:46 +0530

    Style some components

--- src/ui/components/InputComponent.tsx
+++ src/ui/components/InputComponent.tsx
@@ -19,6 +19,25 @@ function setMockResponse(
   });
 }
 
+function Label({ children }: any) {
+  return (
+    <label className="block text-sm text-gray-500 dark:text-gray-300">
+      {children}
+    </label>
+  );
+}
+
+function Input({ type, value, onChange }: any) {
+  return (
+    <input
+      className="block mt-2 w-full placeholder-gray-400/70 dark:placeholder-gray-500 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
+      type={type}
+      value={value}
+      onChange={onChange}
+    ></input>
+  );
+}
+
 function InputComponent() {
   const [op, setOp] = useState(GraphQLOperationType.Query);
   const [name, setName] = useState("");
@@ -51,7 +70,7 @@ function InputComponent() {
   return (
     <div className="flex flex-col">
       <div className="flex">
-        <label>Operation Type</label>
+        <Label>Operation Type</Label>
         <select
           value={op === GraphQLOperationType.Query ? "query" : "mutation"}
           onChange={handleOptionChange}
@@ -60,29 +79,30 @@ function InputComponent() {
           <option value="mutation">Mutation</option>
         </select>
 
-        <label>Operation Name</label>
-        <input type="text" value={name} onChange={handleNameChange} />
+        <div>
+          <Label>Operation Name</Label>
+          <Input type="text" value={name} onChange={handleNameChange} />
+        </div>
 
-        <button onClick={() => setMockResponse(op, name, res, delay)}>
-          Generate Mock Response
+        <button
+          className="px-6 py-2 h-8 rounded-none font-small tracking-wide text-white transition-colors duration-300 transform bg-blue-600  hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          onClick={() => setMockResponse(op, name, res, delay)}
+        >
+          Mock
         </button>
       </div>
 
       <div className="flex">
-        <label className="label nameLabel" htmlFor="delayInput">
-          Response Delay (ms)
-        </label>
-        <input
+        <Label>Response Delay (ms)</Label>
+        <Input
           type="number"
-          className="input-field"
-          id="delayInput"
           value={delay.toString()}
           onChange={handleDelayChange}
         />
       </div>
 
       <div className="flex">
-        <label>Mock Response</label>
+        <Label>Mock Response</Label>
         <textarea
           rows={4}
           value={res}
