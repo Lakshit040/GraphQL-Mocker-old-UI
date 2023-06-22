189b8a612255531e3e6f970cdac65fa83b081f2b
Priyansh Rathi <priyansh.rathi@sprinklr.com>
2023-06-22 19:14:13 +0530

    Combine collapsible and mock response components into a single component

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,12 +1,12 @@
-import CollapsibleMockResponseConfigComponent from "./components/CollapsibleMockResponseConfigComponent";
+import MockResponseConfigComponent from "./components/MockResponseConfigComponent";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-center items-center">
       {/* Toolbar should come here */}
       <div className="h-full w-full flex flex-col items-center p-5">
-        <CollapsibleMockResponseConfigComponent />
-        <CollapsibleMockResponseConfigComponent />
+        <MockResponseConfigComponent />
+        <MockResponseConfigComponent />
       </div>
     </div>
   );
