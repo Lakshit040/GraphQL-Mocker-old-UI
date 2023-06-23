c31092a46af9de3d68feea2214f5ad854618811a
Priyansh Rathi <priyansh.rathi@sprinklr.com>
2023-06-23 18:07:21 +0530

    Add SvgButtonComponent

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,8 @@
 import { useState, useCallback } from "react";
 
 import MockResponseConfigComponent from "./components/MockResponseConfigComponent";
+import SvgButtonComponent from "./components/SvgButtonComponent";
+
 import { guidGenerator } from "../common/utils";
 
 function App() {
@@ -18,15 +20,13 @@ function App() {
         <h2 className="text-gray-900 self-center text-lg tracking-wide">
           Mock GraphQL Requests
         </h2>
-        <svg
+        <SvgButtonComponent
           className="w-8 h-8 p-2 shrink-0 rounded-full ml-auto text-gray-500 hover:bg-gray-100"
-          fill="currentColor"
           viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
           onClick={handleAddButtonPressed}
         >
           <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
-        </svg>
+        </SvgButtonComponent>
       </div>
 
       <div className="grow w-4/5 flex flex-col items-center p-2">
--- src/ui/components/MockResponseConfigComponent.tsx
+++ src/ui/components/MockResponseConfigComponent.tsx
@@ -1,5 +1,7 @@
 import { useState, useCallback } from "react";
+
 import TopAlignedLabelAndInput from "./TopAlignedLabelAndInput";
+import SvgButtonComponent from "./SvgButtonComponent";
 
 import { GraphQLOperationType } from "../../common/types";
 import { backgroundSetMockResponse } from "../helpers/utils";
@@ -110,13 +112,11 @@ const MockResponseConfigComponent = () => {
           isExpanded ? "bg-gray-100" : ""
         }`}
       >
-        <svg
+        <SvgButtonComponent
           className={`w-6 h-6 text-gray-500 shrink-0 ml-1 mr-2 ${
             isExpanded ? "rotate-180" : ""
           }`}
-          fill="currentColor"
           viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
           onClick={handleHeadingClick}
         >
           <path
@@ -124,7 +124,8 @@ const MockResponseConfigComponent = () => {
             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
             clip-rule="evenodd"
           ></path>
-        </svg>
+        </SvgButtonComponent>
+
         <TopAlignedLabelAndInput
           htmlInputId="inputSelectOperationType"
           label="Operation Type"
@@ -152,20 +153,16 @@ const MockResponseConfigComponent = () => {
         />
 
         <div className="grow flex flex-row-reverse">
-          <svg
+          <SvgButtonComponent
             className="w-10 h-10 p-2 ml-1 shrink-0 rounded-full text-gray-500 hover:bg-gray-200"
-            fill="currentColor"
             viewBox="0 0 32 32"
-            xmlns="http://www.w3.org/2000/svg"
             onClick={handleDeleteButtonPressed}
           >
             <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5  c-0.8-0.8-2.1-0.8-2.8,0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,0.2,1,0.6,1.4  C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z" />
-          </svg>
-          <svg
+          </SvgButtonComponent>
+          <SvgButtonComponent
             className="w-10 h-10 p-2 shrink-0 rounded-full text-gray-500 hover:bg-gray-200"
-            fill="currentColor"
             viewBox="0 0 32 32"
-            xmlns="http://www.w3.org/2000/svg"
             onClick={handleMockButtonPressed}
           >
             {areMocking ? (
@@ -173,7 +170,7 @@ const MockResponseConfigComponent = () => {
             ) : (
               <path d="M5.92 24.096q0 1.088 0.928 1.728 0.512 0.288 1.088 0.288 0.448 0 0.896-0.224l16.16-8.064q0.48-0.256 0.8-0.736t0.288-1.088-0.288-1.056-0.8-0.736l-16.16-8.064q-0.448-0.224-0.896-0.224-0.544 0-1.088 0.288-0.928 0.608-0.928 1.728v16.16z" />
             )}
-          </svg>
+          </SvgButtonComponent>
         </div>
       </div>
 
