9d75d807c8d78dbc15f95dfc26402ce06c84238e
Lakshit <lakshit.sharma@sprinklr.com>
2023-07-06 18:57:19 +0530

    added test file for randomDataTypeGenerator file

---
+++ src/background/helpers/__tests__/randomDataTypeGenerator.test.ts
@@ -0,0 +1,57 @@
+import { dynamicValueGenerator } from "../randomDataTypeGenerator";
+import { BooleanType } from "../../../common/types";
+
+describe('dynamicValueGenerator function', () => {
+  const mockEnumTypes = new Map<string, any>();
+  const mockDataSet = {
+    stringLength: 5,
+    arrayLength: 5,
+    numRangeStart: 1,
+    numRangeEnd: 10,
+    digitsAfterDecimal: 2,
+    booleanValues: BooleanType.True,
+    isSpecialAllowed: true
+  };
+
+  mockEnumTypes.set('TestEnum', ['A', 'B', 'C']);
+
+  it('should generate a string for type "String"', () => {
+    const result = dynamicValueGenerator('String', mockEnumTypes, mockDataSet, 'name');
+    expect(typeof result).toBe('string');
+  });
+
+  it('should generate a number for type "Int"', () => {
+    const result = dynamicValueGenerator('Int', mockEnumTypes, mockDataSet, 'age');
+    expect(typeof result).toBe('number');
+  });
+
+  it('should generate a unique string for type "ID"', () => {
+    const result = dynamicValueGenerator('ID', mockEnumTypes, mockDataSet, 'id');
+    expect(typeof result).toBe('string');
+  });
+
+  it('should generate a float for type "Float"', () => {
+    const result = dynamicValueGenerator('Float', mockEnumTypes, mockDataSet, 'price');
+    expect(typeof result).toBe('number');
+  });
+
+  it('should generate a boolean for type "Boolean"', () => {
+    const result = dynamicValueGenerator('Boolean', mockEnumTypes, mockDataSet, 'active');
+    expect(typeof result).toBe('boolean');
+  });
+
+  it('should generate an array for an array type', () => {
+    const result = dynamicValueGenerator('[String]', mockEnumTypes, mockDataSet, 'names');
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  it('should generate a valid enum value for an enum type', () => {
+    const result = dynamicValueGenerator('TestEnum', mockEnumTypes, mockDataSet, 'enumValue');
+    expect(mockEnumTypes.get('TestEnum')).toContain(result);
+  });
+
+  it('should generate a string for an unknown type', () => {
+    const result = dynamicValueGenerator('UnknownType', mockEnumTypes, mockDataSet, 'unknown');
+    expect(typeof result).toBe('string');
+  });
+});
