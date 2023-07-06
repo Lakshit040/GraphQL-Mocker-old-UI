6b09d0cf60562f1ce297d1e8c05148b65b8397f9
Lakshit <lakshit.sharma@sprinklr.com>
2023-07-06 18:52:20 +0530

    Test file added for specificFieldGenerator file

---
+++ src/background/helpers/__tests__/specificFieldGenerator.test.ts
@@ -0,0 +1,46 @@
+import { specificFieldGenerator } from "../specificFieldGenerator";
+
+describe('specificFieldGenerator function', () => {
+  it.each([
+    "name",
+    "username",
+    "gender",
+    "age",
+    "animal",
+    "color",
+    "company",
+    "domain",
+    "email",
+    "ip",
+    "profession",
+    "twitter",
+    "url",
+    "address",
+    "altitude",
+    "areacode",
+    "city",
+    "coordinates",
+    "country",
+    "depth",
+    "latitude",
+    "longitude",
+    "phone",
+    "postal",
+    "postcode",
+    "province",
+    "state",
+    "street",
+    "date",
+    "day",
+    "currency",
+    "birthday"
+  ])('should generate non-undefined values for field "%s"', (field) => {
+    const result = specificFieldGenerator(field);
+    expect(result).toBeDefined();
+  });
+
+  it('should return undefined for an unknown field', () => {
+    const result = specificFieldGenerator("unknown_field");
+    expect(result).toBeUndefined();
+  });
+});
