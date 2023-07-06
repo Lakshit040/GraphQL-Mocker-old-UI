671299269765e172fbb972ede77700a7186e94eb
Lakshit <lakshit.sharma@sprinklr.com>
2023-07-06 19:18:44 +0530

    Added test file for randomMockDataGenerator file

---
+++ src/background/helpers/__tests__/randomMockDataGenerator.test.ts
@@ -0,0 +1,81 @@
+import { parse } from 'graphql';
+import giveRandomResponse from '../randomMockDataGenerator';
+import { BooleanType } from '../../../common/types';
+
+describe('giveRandomResponse function', () => {
+  // Setting up the mock objects
+  const mockFieldTypes = new Map();
+  mockFieldTypes.set('name', 'String');
+  mockFieldTypes.set('age', 'Int');
+  mockFieldTypes.set('address', 'Object');
+  mockFieldTypes.set('active', 'Boolean');
+  mockFieldTypes.set('scores', '[Float]');
+  mockFieldTypes.set('userFragment', 'Fragment');
+
+  const mockEnumTypes = new Map();
+  const mockUnionTypes = new Map();
+  const mockInterfaceTypes = new Map();
+
+  const mockDataSet = {
+    stringLength: 5,
+    arrayLength: 3,
+    numRangeStart: 1,
+    numRangeEnd: 100,
+    digitsAfterDecimal: 2,
+    booleanValues: BooleanType.True,
+    isSpecialAllowed: false,
+  };
+
+  // Test case for Query operation
+  it('should correctly handle Query operation', () => {
+    const queryString = `
+      query {
+        user {
+          name
+          age
+          address
+          active
+          scores
+        }
+      }
+    `;
+
+    const documentNode = parse(queryString);
+
+    const result = giveRandomResponse(documentNode, mockFieldTypes, mockEnumTypes, mockUnionTypes, mockInterfaceTypes, mockDataSet);
+
+    expect(typeof result.user.name).toBe('string');
+    expect(typeof result.user.age).toBe('number');
+    expect(typeof result.user.address).toBe('object');
+    expect(typeof result.user.active).toBe('boolean');
+    expect(Array.isArray(result.user.scores)).toBe(true);
+  });
+
+  it('should correctly handle Mutation operation', () => {
+    const mutationString = `
+      mutation {
+        updateUser(id: 1, data: {
+          name: "New Name"
+          age: 25
+        }) {
+          name
+          age
+          address
+          active
+          scores
+        }
+      }
+    `;
+
+    const documentNode = parse(mutationString);
+
+    const result = giveRandomResponse(documentNode, mockFieldTypes, mockEnumTypes, mockUnionTypes, mockInterfaceTypes, mockDataSet);
+
+    expect(typeof result.updateUser.name).toBe('string');
+    expect(typeof result.updateUser.age).toBe('number');
+    expect(typeof result.updateUser.address).toBe('object');
+    expect(typeof result.updateUser.active).toBe('boolean');
+    expect(Array.isArray(result.updateUser.scores)).toBe(true);
+  });
+
+});
