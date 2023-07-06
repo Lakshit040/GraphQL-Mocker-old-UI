4591815c13a51b1ef622194dd1f3d6896e3e98b6
Priyansh Rathi <priyansh.rathi@sprinklr.com>
2023-07-06 17:51:00 +0530

    Setup basic testing pipeline

---
+++ src/common/__tests__/regex.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "@jest/globals";
+import { CONDITION_REGEX, OBJECT_REGEX, ARRAY_REGEX } from "../regex";
+
+test("condition regex", () => {
+  const expression = "a == b && c == d";
+  const matches = expression.match(CONDITION_REGEX)!;
+  const extractedConditions = matches.map((match) =>
+    match.replace(/[()]/g, "").trim()
+  );
+  expect(extractedConditions).toEqual(["a == b", "c == d"]);
+});
+
+test("object regex", () => {
+  const expression = "a == {b: 1}";
+  const matches = expression.match(OBJECT_REGEX)!;
+  expect(matches[2]).toEqual("{b: 1}");
+});
+
+test("array regex", () => {
+  const expression = "a == [1, 2, 3]";
+  const matches = expression.match(ARRAY_REGEX)!;
+  expect(matches[2]).toEqual("[1, 2, 3]");
+});
