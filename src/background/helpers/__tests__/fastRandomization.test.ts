e66301d229d7017aa52a3a6ed91640fe9a61cbc5
Lakshit <lakshit.sharma@sprinklr.com>
2023-07-07 12:26:26 +0530

    Added test file for fastRandomization file

---
+++ src/background/helpers/__tests__/fastRandomization.test.ts
@@ -0,0 +1,42 @@
+import { fastRandomize } from '../fastRandomization';
+import { getQueryEndpoint, getSchema } from '../chromeStorageOptions';
+
+// Create typecasting
+const mockedGetQueryEndpoint = getQueryEndpoint as jest.MockedFunction<typeof getQueryEndpoint>;
+const mockedGetSchema = getSchema as jest.MockedFunction<typeof getSchema>;
+
+jest.mock('../chromeStorageOptions');
+
+describe('fastRandomize', () => {
+  beforeEach(() => {
+    // Use the mocked functions
+    mockedGetQueryEndpoint.mockClear();
+    mockedGetSchema.mockClear();
+  });
+
+  it('should return undefined when getQueryEndpoint returns undefined', async () => {
+    mockedGetQueryEndpoint.mockResolvedValueOnce(undefined);
+    expect(await fastRandomize('testId')).toBeUndefined();
+  });
+
+  it('should return undefined when getSchema returns undefined', async () => {
+    mockedGetQueryEndpoint.mockResolvedValueOnce('query__endpoint');
+    mockedGetSchema.mockResolvedValueOnce(undefined);
+    expect(await fastRandomize('testId')).toBeUndefined();
+  });
+
+  it('should return error response when query parsing fails', async () => {
+    mockedGetQueryEndpoint.mockResolvedValueOnce('invalid query__endpoint');
+    mockedGetSchema.mockResolvedValueOnce('type Query { name: String }');
+    const result = await fastRandomize('testId');
+    expect(result.message).toBe('ERROR_GENERATING_RANDOM_RESPONSE');
+  });
+
+  it('should return success response when query parsing is successful', async () => {
+    mockedGetQueryEndpoint.mockResolvedValueOnce('query { name }__endpoint');
+    mockedGetSchema.mockResolvedValueOnce('type Query { name: String }');
+    const result = await fastRandomize('testId');
+    expect(result.message).toBe('SUCCESS');
+    expect(typeof result.data.name).toBe('string');
+  });
+});
